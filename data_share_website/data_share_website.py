239decfc42d5c4a0e172e1c9405fc7127a909625
Dawid Sielski <dawid.sielski@outlook.com>
2019-02-16 10:54:41 +0100

    test 1

--- data_share_website/data_share_website.py
+++ data_share_website/data_share_website.py
@@ -437,17 +437,18 @@ def check_key():
 def check_node():
     if request.method == 'POST':
         data = request.json
+        print(data)
 
-        try:
-            with open(os.path.join('nodes', 'public.{}.key'.format(data['request_node'])), 'r') as file:
-                public_key = file.read()
-        except FileNotFoundError as e:
-            data_sharing_logger.exception("Remote node check failed. Request: {}".format(data))
-            data_sharing_logger.exception(e)
-            abort(401)
-
-        if not DataShare.validate_signature_from_message(data, public_key=public_key):
-            abort(401)
+        # try:
+        #     with open(os.path.join('nodes', 'public.{}.key'.format(data['request_node'])), 'r') as file:
+        #         public_key = file.read()
+        # except FileNotFoundError as e:
+        #     data_sharing_logger.exception("Remote node check failed. Request: {}".format(data))
+        #     data_sharing_logger.exception(e)
+        #     abort(401)
+        #
+        # if not DataShare.validate_signature_from_message(data, public_key=public_key):
+        #     abort(401)
 
         return 'Success', 200
     return abort(401)
