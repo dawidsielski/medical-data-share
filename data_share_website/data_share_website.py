e11f47bc1b3cfbff9f156ddc8c9334a758fe8f30
Dawid Sielski <dawid.sielski@outlook.com>
2019-02-17 23:27:24 +0100

    Logger update.

--- data_share_website/data_share_website.py
+++ data_share_website/data_share_website.py
@@ -446,7 +446,7 @@ def check_node():
             abort(401)
 
         if not DataShare.validate_signature_from_message(data, public_key=public_key):
-            print("Not validated message.")
+            data_sharing_logger.info('Check node not valid for data: {}'.format(data))
             abort(401)
 
         return 'Success', 200
