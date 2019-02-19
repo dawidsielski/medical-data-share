1bd613d6a4cd27d8cd0a695290e77929b803b7f3
Dawid Sielski <dawid.sielski@outlook.com>
2019-02-19 12:53:27 +0100

    Logger update.

--- utils/nodes_key_pair_updator/NodesKeyPairUpdator.py
+++ utils/nodes_key_pair_updator/NodesKeyPairUpdator.py
@@ -1,6 +1,8 @@
 import os
 import requests
+import logging
 
+from logging.handlers import TimedRotatingFileHandler
 from urllib.parse import urljoin
 from configparser import ConfigParser
 
@@ -13,6 +15,18 @@ key_path = lambda name: os.path.join('keys', name)
 
 config = ConfigParser()
 config.read(os.path.join(os.getcwd(), 'config.ini'), encoding='utf-8')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(funcName)s:%(message)s')
+
+website_file_rotating_handler = TimedRotatingFileHandler('logs/node_updates.log', when="midnight", interval=1)
+website_file_rotating_handler.setLevel(logging.INFO)
+website_file_rotating_handler.setFormatter(formatter)
+website_file_rotating_handler.suffix = "%Y-%m-%d"
+
+logger.addHandler(website_file_rotating_handler)
 
 
 class NodeKeyPairUpdator(object):
@@ -32,11 +46,12 @@ class NodeKeyPairUpdator(object):
 
         os.remove(key_path('public.old.key'))
         os.remove(key_path('private.old.key'))
-        print('New_keys_generated')
+        logger.info('New_keys_generated')
 
     @staticmethod
     def update_key_on_available_nodes():
         available_nodes = NodesChecker.get_all_nodes_availability()
+        logger.info(available_nodes)
 
         for key, value in available_nodes.items():
             if value['availability']:
@@ -53,3 +68,4 @@ class NodeKeyPairUpdator(object):
                 data.update({'signature': DataShare.get_signature_for_message(data, filename='private.old.key').decode()})
 
                 r = requests.post(url, json=data)
+                logger.info('{} {} {} {}'.format(key, url, r.status_code, data))
